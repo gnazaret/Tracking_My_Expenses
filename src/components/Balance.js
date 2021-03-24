4dba8a6863015546e742ca2a2974427d96a6356e
German <gnazaret@gmail.com>
2021-03-23 21:35:33 -0400

    installed depencies and created components(Header, Balance and Income) to start the project'

---
+++ src/components/Balance.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Balance = () => {
+    return (
+        <>
+
+                <h2>Your Balance Is</h2>
+                <h1 id="balance">$0.00</h1>
+        </>
+    )
+}
+
+export default Balance
---
+++ src/components/Income.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Income = () => {
+    return (
+        <div className='income-container'>
+
+            <div>
+                <h3>Income</h3>
+            </div>
+            <div>
+                <h3>Expenses</h3>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Income
