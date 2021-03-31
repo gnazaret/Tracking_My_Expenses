d80264f5cbaf398690ce7c1deb7b5d652198203f
German <gnazaret@gmail.com>
2021-03-30 21:15:12 -0400

    Arthur explained and helped me using createcontext hook

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import GlobalContext from '../context/GlobalState';
 
 const Transactions = () => {
+    const context = useContext(GlobalContext);
+
+    console.log(context);
+
     return (
         <div>
             <h2>History</h2>
             <ul className="list">
                 <li className="minus">
-                    Cash <span>-$400</span> <button className="delete-btn">X</button>
+                    Cash <span>-$100</span> <button className="delete-btn">X</button>
                 </li>
             </ul>
         </div>
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,38 +1,43 @@
 import React, { createContext, useReducer } from 'react';
-import AppReducer from './AppReducer'
+// import AppReducer from './AppReducer'
 
 
 
-const initialState = {
-    transactions: [
-        { id: 1, text: 'Comic Books', amount: -40},
-        { id: 2, text: 'Groceries', amount: -80},
-        { id: 3, text: 'Griddle', amount: -150},
-        { id: 4, text: 'Salary', amount: 2000}
-    ]
+// const operations = {
+//     transactions: [
+//         { id: 1, text: 'Comic Books', amount: -40},
+//         { id: 2, text: 'Groceries', amount: -80},
+//         { id: 3, text: 'Griddle', amount: -150},
+//         { id: 4, text: 'Salary', amount: 2000}
+//     ]
     
-}
+// }
 
 
-//Create context
+// //Create context
 
-const GlobalContext = createContext(initialState);
+ const GlobalContext = createContext(null);
+
+export default GlobalContext 
+
+// //Provider component
+
+// const GlobalProvider = ( { children }) => {
+
+    
+
+//     const [ state, dispatch ] = useReducer(AppReducer, operations);
 
 
-//Provider component
-
-const GlobalProvider = ( { children }) => {
-    const [ state, dispatch ] = useReducer(AppReducer, initialState);
-
-
-    return (<GlobalContext value={{
-        transactions: state.transactions
-    }
+//     return (<GlobalContext.Provider value={{
+//         transactions: state.transactions
+//     }
         
-    }>
-        {children}
-    </GlobalContext>)
+//     }>
+//         {children}
+//     </GlobalContext.Provider>)
 
-}
+// }
 
-export default GlobalProvider;
+// export default GlobalProvider;
+
